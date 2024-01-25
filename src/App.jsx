8f30eb53ac2751c63e7d2c4f835bde79b58f9286
ClemiltonCSilva <157602498+ClemiltonCSilva@users.noreply.github.com>
2024-01-25 15:01:25 +0000

    criacao de componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,11 @@
 import './App.css'
+import Card from './components/Card/Card'
 
 function App() {
   return (
     <>
-      <div className="card">
-        <h2> Rich Sanchez</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
-      <div className="card">
-        <h2> Sammer Smith</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
-      <div className="card">
-        <h2> Morty Smith</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
-      <div className="card">
-        <h2> Beth Smith</h2>
-        <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
-      </div>
+     <Card /> 
+     <Card />
     </>
   )
 }
