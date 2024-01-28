9580bf9d0b08d2067a0ece63649d300657ff7b2e
ClemiltonCsilva <clemiltonsilva@hotmail.com>
2024-01-28 19:42:42 +0000

    DESAFIO 1

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -2,10 +2,23 @@ function Card() {
     return (
         <>
           <div className="card">
+          <h2> NOME</h2>
             <h2> Rich Sanchez</h2>
-            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />  
           </div>
-         
+          <div className="card">
+            <h2> Testando </h2>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />  
+          </div>
+          <div className="card">
+            <h2> Testando 1</h2>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />  
+          </div>
+          <div className="card">
+            <h2> Testando 2</h2>
+            <img src="https://rickandmortyapi.com/api/character/avatar/1.jpeg" />  
+          </div>
+
         </>
       )
 
