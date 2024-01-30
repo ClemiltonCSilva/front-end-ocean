43b3eae469d03cf3e5d7a8af0e426ad6924e08bf
ClemiltonCsilva <clemiltonsilva@hotmail.com>
2024-01-30 20:01:27 +0000

    aplicação do map

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,36 @@ import './App.css'
 import Card from './components/Card/Card'
 
 function App() {
-
   const item1 = {
     name: "Rick Sanchez",
-    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"  
+    image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg"    
   }
-
   const item2 = {
     name: "Morty Smith",
     image: "https://rickandmortyapi.com/api/character/avatar/2.jpeg"  
   }
-
   const item3 = {
     name: "Summer Smith",
     image: "https://rickandmortyapi.com/api/character/avatar/3.jpeg"  
   }
+  const item4 = {
+    name: "Beth Smith",
+    image: "https://rickandmortyapi.com/api/character/avatar/4.jpeg"  
+  }
+
+  const   lista_card = [item1, item2, item3, item4] 
+ 
   return (
-    <>
+
+   <>
+      
       <div className='cards'>
-        <Card item={item1} /> 
+        
+        {/*<Card item={item1} /> 
         <Card item={item2} />
         <Card item={item3} />
+        <Card item={item4} />*/}
+       {lista_card.map(item => <Card item={item}/>)} 
       </div>
     </>
   )
